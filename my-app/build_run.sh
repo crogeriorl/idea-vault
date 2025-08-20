2720e1ada626514472f9120bed00a82d55bc81ec
Rogerio Lopes <crogeriolopes@gmail.com>
2025-08-19 23:15:39 -0300

    remote run script

---
+++ my-app/build_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo # Updating local code from GitHub repo
+git pull
+
+echo "> deleting old version."
+_ctr=$(docker ps -q)
+docker container stop $_ctr
+docker container rm $_ctr
+
+echo ## Bulding image...
+docker build -t my-app:$1 .
+
+echo ### Run app on docker!
+docker run -d -p 8080:3000 my-app:v1
