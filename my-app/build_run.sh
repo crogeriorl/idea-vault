2d5f882f544aafb818dab9a23f0fb5b4c57463b4
Rogerio Lopes <crogeriolopes@gmail.com>
2025-08-20 21:20:10 -0300

    final pipe

--- my-app/build_run.sh
+++ my-app/build_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo # Updating local code from GitHub repo
-git pull
+# git pull
 
-echo "> deleting old version."
 _ctr=$(docker ps -q)
 docker container stop $_ctr
 docker container rm $_ctr
 
-echo ## Bulding image...
-docker build -t my-app:$1 .
+docker login ghcr.io --username crogeriorl --password $1
+echo ">> Pulling app image..."
+docker pull $2
 
-echo ### Run app on docker!
-docker run -d -p 8080:3000 my-app:$1
+echo ## Run app on docker!
+docker run -d -p 8080:3000 $2
